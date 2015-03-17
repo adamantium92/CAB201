0f1b67ea73da3527745ed2d601a53cd4d873d727
Adam <adam.gibbon92@gamil.com>
2015-03-17 12:02:11 +1000

    Week 4 tutorial work

---
+++ Tutorials/Week 4/Activity 2/ConsoleApplication1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linear_Search
+{
+
+    /// <summary>
+    /// 
+    /// Demonstrates a Linear Search on an array of strings
+    ///  
+    /// author: Mike Roggenkamp
+    /// 
+    /// date: March 2009
+    /// 
+    /// </summary>
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            const string exitMessage = "\n Key any key to exit program ... ";
+
+            string[] words = {"the", "quick", "brown", "fox", 
+                              "jumps", "over", "the", "lazy", "dog"};
+
+            int location;
+
+            DisplayArray(words);
+
+            location = LinearSearch(words, "the");
+            OutputSearchResult(location, "the");
+
+            location = LinearSearch(words, "able");
+            OutputSearchResult(location, "able");
+
+
+            location = LinearSearch(words, "over");
+            OutputSearchResult(location, "over");
+
+            location = LinearSearch(words, "zebra");
+            OutputSearchResult(location, "zebra");
+
+            location = LinearSearch(words, "dog");
+            OutputSearchResult(location, "dog");
+
+            OutputMessage(exitMessage);
+
+        } //end Main
+
+        /// <summary>
+        ///  linear search of "words" for "word" 
+        /// </summary>
+        /// <param name="words">list of words to be searched</param>
+        /// <param name="word">the word being searchedd for</param>
+        /// <returns> position of "word" in "words" if it is there
+        ///             otherwise returns -1    </returns>
+        static int LinearSearch(string[] words, string word) {
+            
+            int position = 0;
+
+            while ((position < words.Length) && (words[position] != word))
+            {
+                position += 1;
+            } // end while
+            
+            if (position < words.Length){
+                return position;  // found it
+            } else {
+                return -1; // key not in the list
+            }
+        } //end LinearSearch
+
+
+        /// <summary>
+        /// Displays the elements of the array "words"
+        /// </summary>
+        /// <param name="words">array to be displayed</param>
+        static void DisplayArray(string[] words)
+        {
+
+            foreach (string element in words)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
+
+        }//end DisplayArray
+
+        /// <summary>
+        /// Displays the outcome of the linear search
+        /// </summary>
+        /// <param name="position">either the position of "word" if it was found
+        ///                         or -1 if "word" was not found</param>
+        /// <param name="word"></param>
+        static void OutputSearchResult(int position, string word)
+        {
+            string continueMessage = "\nPress any key to continue:";
+
+            if (position < 0)
+            {
+                Console.WriteLine("\tthe word \"{0}\" not found in the list\n ", word);
+            }
+            else
+            {
+                Console.WriteLine("\tthe word \"{0}\" found in postion {1} in the list\n", word, position);
+            }
+
+            OutputMessage(continueMessage);
+
+        } //end OutputSearchResult
+
+        /// <summary>
+        /// Outputs the string "s"
+        /// </summary>
+        /// <param name="s">String to be output</param>
+        static void OutputMessage(string s)
+        {
+
+            Console.Write(s);
+            Console.ReadKey();
+
+        }// end OutPutMessage
+
+    }
+}
---
+++ Tutorials/Week 4/Activity 3/ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersDuplicate = {1, 1, 2, 8, 6, 7, 8, 9, 10, 6, 11};
+            int[] numbers = {1, 2, 3, 8, 6, 7, 10};
+            DisplayArray(numbers);
+            Console.WriteLine("Does the array contain duplicates? " + FindDuplicates(numbers));
+            DisplayArray(numbersDuplicate);
+            Console.WriteLine("Does the array contain duplicates? " + FindDuplicates(numbersDuplicate));
+            Console.ReadLine();
+        }
+
+        static bool FindDuplicates(int[] numbers) {
+            
+            // Find the first element
+            // Search each element in the array 
+            // If no match repeat until everything has been searched
+            // If match return true
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] == numbers[j]) return true;
+                }
+            }
+
+            return false;
+           
+        } //end FindDuplicates
+
+        static void DisplayArray(int[] numbers)
+        {
+
+            foreach (int element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine();
+
+        }//end DisplayArray
+}
+}
